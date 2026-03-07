a1eac89598ce5005edfadcd4290730ee6cf247dd
Lorberg Agent <agent.lorberg@web.de>
2026-03-07 15:55:49 +0100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.lorberg.rechnung',
+  appName: 'rechnung-scanner',
+  webDir: 'www'
+};
+
+export default config;
